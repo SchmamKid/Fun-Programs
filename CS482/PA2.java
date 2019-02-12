89af3467422f42e141e404f97bdf1614759c5cc1
SchmamKid <sam.martins414@yahoo.com>
2019-02-11 23:07:43 -0500

    mm

--- CS482/PA2.java
+++ CS482/PA2.java
@@ -36,7 +36,7 @@ public static void main(String [] args) throws InvalidKeyException, FileNotFound
 
     byte [] cipherText = new byte[cipherTextString.length() / 2];
     for(int i = 0; i < cipherText.length; i++){
-        cipherText[i] = (byte) Byte.parseByte(cipherTextString.substring(i*2, i *2 + 2), 16);
+        cipherText[i] = (byte) Integer.parseInt("0x" + cipherTextString.substring(i*2, i *2 + 2));
 
     }
     //byte[] cipherText = cipherTextString.getBytes();
