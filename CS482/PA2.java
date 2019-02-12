27dff55796ab39bb5f7b87afb5519db873db022a
SchmamKid <sam.martins414@yahoo.com>
2019-02-11 22:26:24 -0500

    fixed index hopefully

--- CS482/PA2.java
+++ CS482/PA2.java
@@ -20,53 +20,53 @@ public static void main(String [] args) throws InvalidKeyException, FileNotFound
 
     PrintWriter printWriter = new PrintWriter(new FileOutputStream(savedText, true));
 
-    byte[] inKey = new byte[16];
+  //  byte[] inKey = new byte[16];
 
-    inKey[15] = (byte) 0x03;
-    for(int i = 14; i > 4; i--){
-        inKey[i] = (byte) 0x00;
-    }
+    //inKey[15] = (byte) 0x03;
+    //for(int i = 14; i > 4; i--){
+    //    inKey[i] = (byte) 0x00;
+  //
+
+    byte [] inKey = {(byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+        (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03};
 
 
     String cipherTextString = "8F9C9C3EC872D10E8C955CFE5D0672716A9A7C285876B94A6BD3133193E67DB7C2D0278FAC5499898389EC1A5F8C9B247530D564DECEC99B829D7CC45EAB3EFFEE9B2639AF76033641E86E67A5F80564";
 
     byte[] cipherText = cipherTextString.getBytes();
+  //  System.out.println(AESExample.convertToString(cipherText));
+
 
     int numOfCiphertextBlocks = cipherText.length / 16 - 1; // Each AES block has 16 bytes and we need to exclude the IV
 	  byte[] cleartextBlocks = new byte[numOfCiphertextBlocks * 16];
 
     byte[] receivedIV = new byte[16];
-    for (int i = 0; i < 16; i++) receivedIV[i] = cipherText[i];
+    for (int i = 0; i < 16; i++){
+      receivedIV[i] = cipherText[i];
+    }
 
-    int lowerBound = 32;
-    int upperBound = 127;
+    int lowerBound = 31;
+    int upperBound = 128;
 
     byte[] currentDecryptionBlock = new byte[16];
 
     //long value = 0x0000000000000;
     BigInteger value = new BigInteger(args[0], 16);
-
     //long max = 0xFFFFFFFF;
     BigInteger max = new BigInteger(args[1],16);
-
     BigInteger one = new BigInteger("1");
-
     long start = System.currentTimeMillis();
 
   //  while((value & 0xfffffff) < (max & 0xfffffff))
     while(value.compareTo(max) < 0){
 
-        //System.out.printf("%x\n", value);
-
-        //System.out.println ("As a string: " + new String (inKey));
-		//for (byte k : inKey) System.out.print (String.format("%02X", k & 0xff));
-		//System.out.println (System.getProperty ("line.separator"));
-        //System.out.println(Arrays.toString(inKey));
         byte [] hexnums = value.toByteArray();
         //byte [] hexnums = ByteBuffer.allocate(4).putInt(value).array();
-
-        for(int x = 0; x < hexnums.length; x++){
-            inKey[4 - x] = hexnums[x];
+        int index = 3;
+        for(int x = hexnums.length; x > 0; x--){
+            inKey[index] = hexnums[x];
+            index--;
             //inKey[x] = (byte) inKey[x];
         }
 
