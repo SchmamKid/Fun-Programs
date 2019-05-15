38d7fd70d1ab37490594b5194bf739e3c5989ccd
SchmamKid <sam.martins414@yahoo.com>
2019-05-14 23:37:08 -0400

    1

--- Alien.py
+++ Alien.py
@@ -1,19 +1,18 @@
-import sys
 
 import pygame
 from Settings import Settings
+from Ship import Ship
+import game_functions as gf
 def run_game():
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (125,235,173)
+    game_settings.bg_color = (0,2,100)
+    ship = Ship(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(game_settings.bg_color)
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(game_settings,screen,ship)
 
 run_game()
---
+++ game_functions.py
@@ -0,0 +1,11 @@
+import sys
+import pygame
+
+def check_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+def update_screen(settings, screen, ship):
+    screen.fill(settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
