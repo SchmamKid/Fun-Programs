52eb495de3d98765979c6ab04d5167a5e65854c4
SchmamKid <47050245+SchmamKid@users.noreply.github.com>
2019-11-23 15:42:58 -0500

    stuff

--- receipt.py
+++ receipt.py
@@ -1,9 +1,13 @@
+#First install pytesseract
 try:
     from PIL import Image
 except ImportError:
     import Image
 import pytesseract
+#This is for Windows and shows where I store my library.
+#this may not be needed for you but it was for me
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\schma\AppData\Local\Tesseract-OCR\tesseract.exe'
-print(pytesseract.image_to_string(Image.open('test.jpg')))
-print(pytesseract.image_to_string(Image.open('test2.jpg'),lang ='jpn_vert'))
+print(pytesseract.image_to_string(Image.open('test.jpg')))#Prints English String
+print(pytesseract.image_to_string(Image.open('test2.jpg'),lang ='jpn_vert'))#Prints Japanese String
 print(pytesseract.image_to_data(Image.open('test2.jpg'),lang ='jpn'))
+#Prints Data of the image
